5cf399b4a666f4553e16d8a536ce62c92e9466ec
Yueran <yueran.wu@my.jcu.edu.au>
2020-04-25 19:54:28 +0800

    Finish program that print state code and their names

--- state_names.py
+++ state_names.py
@@ -1,3 +1,22 @@
 """
-CP1404 Practical 05
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
 """
+
+# Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ")
+
+for code in CODE_TO_NAME:
+    print(code, "is", CODE_TO_NAME[code])
+
